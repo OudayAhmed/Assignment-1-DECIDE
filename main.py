b292a94730c8d7cc1f570988145c008a74c56840
OudayAhmed <oydddua@gmail.com>
2023-01-26 20:43:54 +0100

    Fix issue #5
    Created method 2 for CMV and added it to the main file.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 #constant
-pi = math.pi
+PI = math.pi
 
 
 
@@ -44,6 +44,25 @@ def CMV_0(POINTS):
         if math.sqrt((POINTS[i][0]**2 - POINTS[i+1][0]**2) +  (POINTS[i+1][1]**2 - POINTS[i+1][1] ** 2)) > LENGTH1:
             return True
 
+def CMV_2(POINTS, EPSILON, PI):
+    if not (0 <= EPSILON < PI):
+        raise ValueError("EPSILON must be between 0 and PI")
+    for i in range(len(POINTS) - 2):
+        point_1_x = POINTS[i][0]
+        point_1_y = POINTS[i][1]
+        point_2_x = POINTS[i+1][0]
+        point_2_y = POINTS[i+1][1]
+        point_3_x = POINTS[i+2][0]
+        point_3_y = POINTS[i+2][1]
+        if not(point_1_x == point_2_x and point_1_y == point_2_y) or not(point_3_x == point_2_x and point_3_y == point_2_y):
+            u = [point_1_x - point_2_x, point_1_y - point_2_y]
+            v = [point_3_x - point_2_x, point_3_y - point_2_y]
+            angle = np.arccos(np.dot(u/np.linalg.norm(u), v/np.linalg.norm(v)))
+            if (angle < PI-EPSILON) or (angle > PI+EPSILON):
+                return True
+    return False
+
+
 def CMV_5(POINTS):
     NUMPOINTS = len(POINTS)
     for i in range(NUMPOINTS-2):
@@ -51,20 +70,18 @@ def CMV_5(POINTS):
             if (POINTS[j] - POINTS[i]) < 0:
                 return True
 
-
-
 def CMV_4(POINTS):
     NUMPOINTS = len(POINTS)
     quadrant = [(0,0),(-1,0),(0,-1),(1,0)]
     last_quad = None
-    
+
     for i in range(NUMPOINTS-1):
         if POINTS[i][0] >= 0:
             if POINTS[i][1] >= 0:
                 if last_quad != quadrant[0]:
                     if i != 0:
                         return True
-                else: 
+                else:
                     last_quad = quadrant[0]
             else:
                 if last_quad != quadrant[3]:
@@ -72,30 +89,26 @@ def CMV_4(POINTS):
                         return True
                     else:
                         last_quad = quadrant[3]
-                else: 
+                else:
                     last_quad = quadrant[3]
-                
+
         if POINTS[i][0] < 0:
             if POINTS[i][1] >= 0:
                 if last_quad != quadrant[1]:
                     if i != 0:
                         return True
-                    else: 
+                    else:
                         last_quad = quadrant[1]
-                else: 
+                else:
                     last_quad = quadrant[1]
-            else: 
+            else:
                 if last_quad != quadrant[2]:
                     if i != 0:
                         return True
-                    else: 
+                    else:
                         last_quad = quadrant[2]
                 else:
                     last_quad = quadrant[2]
-    
-            
-        
-            
 
 
 def DECIDE():
