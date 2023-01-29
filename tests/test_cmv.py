3a9e269633600fdb17f52e1e3c99efc514574a85
Elin <eliu@kth.se>
2023-01-29 15:27:47 +0100

    Update documentation for tests
    Rewrite documentation for tests to match the current structure used.

--- tests/test_cmv.py
+++ tests/test_cmv.py
@@ -5,33 +5,21 @@ from src.cmv import CMV
 
 class TestCMV(TestCase):
     def test_cmv5_negative(self):
-        """Test case for CMV5 to check if the output is false when it should be false.
-
-        Inputs are length of array containing datapoints and datapoints.
-        Xi < Xj when it should be Xj < Xi.
-        """
+        """Test case for CMV5 method with false sets of data points."""
 
         points = [[0.2, 0.3], [0.4, 0.2], [0.3, 0.1]]
         numpoints = 3
         self.assertFalse(CMV.cmv5(numpoints, points))
     
     def test_cmv5_positive(self):
-        """Test case for CMV5 to check if the output is true when it should be true.
-
-        Inputs are length of array containing datapoints and datapoints.
-        Xj < Xi.
-        """
+        """Test case for CMV5 method with at least one set of positive data points."""
         
         points = [[0.4, 0.3], [0.2, 0.2], [0.1, 0.3]]
         numpoints = 3
         self.assertTrue(CMV.cmv5(numpoints, points))
 
     def test_cmv5_invalid(self):
-        """Test case for CMV5 to check if the output is false when it should be false.
-
-        Inputs are length of array containing datapoints and datapoints.
-        Input is invalid to to check if input is properly validated.
-        """
+        """Test case for CMV5 method with invalid data points."""
         
         points = [[0.4, 0.3]]
         numpoints = 1
