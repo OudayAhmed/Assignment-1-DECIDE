cb484fb35a01bc8a56a64bd8ebe878ad4641da06
OudayAhmed <oydddua@gmail.com>
2023-01-29 15:57:56 +0100

    Fix issue#40
    Added the documentation for cmv2 method.
    Created three test cases (positive, negative, invalid data) for cmv2 method and added the documentation for them.

--- tests/test_cmv.py
+++ tests/test_cmv.py
@@ -4,4 +4,20 @@ from src.cmv import CMV
 
 
 class TestCMV(TestCase):
-    pass
+    def test_cmv_2_positive(self):
+        """Positive test for cmv2 method"""
+        points = [[0.1, 0.2], [0.3, 0.1], [0.2, 0.3]]
+        epsilon = 0.5
+        self.assertTrue(CMV.cmv2(points, epsilon))
+
+    def test_cmv_2_negative(self):
+        """Negative test for cmv2 method"""
+        points = [[0.1, 0.2], [0.3, 0.1], [0.2, 0.3]]
+        epsilon = 3
+        self.assertFalse(CMV.cmv2(points, epsilon))
+
+    def test_cmv_2_negative_epsilon(self):
+        """Test case for cmv2 method with negative EPSILON"""
+        points = [[0.1, 0.2], [0.3, 0.1], [0.2, 0.3]]
+        epsilon = -0.3
+        self.assertFalse(CMV.cmv2(points, epsilon))
