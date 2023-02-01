5bbbaaeb1fd90e7d1879e87b4a5cf94d2aeedc08
oscarknowles <oscar@knowles.se>
2023-02-01 16:21:14 +0100

    Fix issue#80
    Created positiveInput.yml
    
    Renamed input.yml to negativeInput.yml
    
    Changed the input yaml in main

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from main.decide import Decide
 
 
 def main():
-    with open("tests/input/input.yml", 'r') as f:
+    with open("tests/input/positiveInput.yml", 'r') as f:
         input = yaml.safe_load(f)
     if Decide(input).decide() is True:
         print("YES")
